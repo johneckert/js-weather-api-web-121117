17d3f6d8f5cc93989be32593db8b62b67d16f768
johneckert <johnteckert@gmail.com>
2018-02-09 14:37:43 -0500

    done

--- js/init.js
+++ js/init.js
@@ -1,12 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var API_KEY = "Go get an API key";
-  var CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
-  var URL = CORS_WRAPPER + "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
-  var ctx = document.getElementById("NYCWeatherChart").getContext("2d")
+document.addEventListener("DOMContentLoaded", function() {
+  var API_KEY = "17acec1b29fa747b";
+  var CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/";
+  var URL =
+    CORS_WRAPPER +
+    "http://api.wunderground.com/api/" +
+    API_KEY +
+    "/hourly/q/NY/New_York.json";
+  var ctx = document.getElementById("NYCWeatherChart").getContext("2d");
 
+  // makeRequest(URL, function(json) {
+  //   var data = generateDataSet(getHours(json), getFahrenheits(json));
+  //   var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
+  // });
 
-  makeRequest(URL, function(json) {
-    var data = generateDataSet(getHours(json), getFahrenheits(json));
-    var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
-  });
+  fetch(URL)
+    .then(request => request.json())
+    .then(json => {
+      console.log("gethours: ", getHours(json), "getF: ", getFahrenheits(json));
+      var data = generateDataSet(getHours(json), getFahrenheits(json));
+      var ctx = document.getElementById("NYCWeatherChart").getContext("2d");
+      var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
+      // var tempChart = new Chart(ctx, {
+      //   type: "line",
+      //   data: data,
+      //   options: { bezierCurve: true }
+      // });
+
+      console.log("json:", json);
+      console.log("data:", data);
+      console.log("tempChart:", tempChart);
+    });
 });
--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,13 +1,43 @@
-function getFahrenheits(result){
-  // Your code goes here
+function getFahrenheits(result) {
+  return result.hourly_forecast.map(function(hourObj) {
+    return hourObj["FCTTIME"]["hour"];
+  });
 }
 
-function getHours(result){
-  // Your code goes here
+function getHours(result) {
+  return result.hourly_forecast.map(function(hourObj) {
+    return hourObj["temp"]["english"];
+  });
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  //hours = labels  f = data
+  return {
+    labels: data,
+    datasets: [
+      {
+        label: "Hours",
+        data: labels,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)"
+        ],
+        borderColor: [
+          "rgba(255,99,132,1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)"
+        ],
+        borderWidth: 1
+      }
+    ]
+  };
 }
 
 function makeRequest(endpoint, success) {
